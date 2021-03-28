a83d134bc0763d07b7580dfddba575bcace9c6da
Nelson Barrios <Nelson.barrios3@gmail.com>
2021-03-27 23:46:27 -0300

    Create calculador.js

---
+++ js/calculador.js
@@ -0,0 +1,35 @@
+let dividir  = function(a1, a2){
+    if (a1 === 0 || a2 === 0){
+        return 'Error'
+    }else{
+        return a1/a2;
+    }
+}
+console.log(dividir(a1, a2));
+
+let multiplicar  = function(b1, b2){
+    if (b1 === 0 || b2 === 0){
+        return 'Error'
+    }else{
+        return b1*b2;
+    }
+}
+console.log(multiplicar(c1, c2));
+
+let sumar  = function(c1, c2){
+    if (c1 === 0 || c2 === 0){
+        return 'Error'
+    }else{
+        return c1+c2;
+    }
+}
+console.log(sumar(c1, c2));
+
+let restar  = function(d1, d2){
+    if (d1 === 0 || d2 === 0){
+        return 'Error'
+    }else{
+        return d1/d2
+    }
+}
+console.log(dividir(d1, d2));
