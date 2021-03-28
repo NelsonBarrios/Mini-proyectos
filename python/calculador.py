4dea89fb34803e692e5d4ee43f9cbed3e50d876b
Nelson Barrios <Nelson.barrios3@gmail.com>
2021-03-28 00:35:03 -0300

    Create calculador.py

---
+++ python/calculador.py
@@ -0,0 +1,23 @@
+a1=float(input("Intro número uno: "))
+a2=float(input("Intro numero dos: "))
+suma=a1+a2
+
+print("La suma es: ",suma)
+
+b1=float(input("Intro número uno: "))
+b2=float(input("Intro numero dos: "))
+resta=b2-b2
+
+print("La resta es: ",resta)
+
+c1=float(input("Intro número uno: "))
+c2=float(input("Intro numero dos: "))
+division=n1/c2
+
+print("La división es: ",division)
+
+d1=float(input("Intro número uno: "))
+d2=float(input("Intro numero dos: "))
+multiplicacion=d1*d2
+
+print("La multiplicación es: ", multiplicacion)
